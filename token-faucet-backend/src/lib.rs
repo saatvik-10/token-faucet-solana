f0acfe34da945f9a8b0df64e86faaa3336b99bf8
saatvik-10 <saatvikmadan529@gmail.com>
2025-07-30 23:22:38 +0530

    added faucet instruction

--- token-faucet-backend/src/lib.rs
+++ token-faucet-backend/src/lib.rs
@@ -1,17 +1,16 @@
 use borsh::{BorshDeserialize, BorshSerialize};
-
 use solana_program::{
     account_info::{AccountInfo, next_account_info},
     entrypoint,
     entrypoint::ProgramResult,
+    example_mocks::solana_sdk::system_instruction,
     msg,
     program::invoke_signed,
     program_error::ProgramError,
-    // program_pack::Pack,
     pubkey::Pubkey,
     sysvar::{Sysvar, rent::Rent},
 };
-use spl_token::pack::Pack;
+use spl_token::solana_program::program_pack::Pack;
 use spl_token::state::Mint;
 
 //use claimed records stored in PDA
@@ -94,8 +93,63 @@ fn process_instruction(
             let system_program = next_account_info(accounts_iter)?;
 
             //validate that token mint is actually a mint account (account that stores global metadata about a token)
-            let mint_data = Mint::unpack(&token_mint_account.data.borrow())?;
-            msg!("Token mint validated. Supply {}", mint_data.supply)
+            let mint_data = Mint::unpack(&token_mint_account.data.borrow())
+                .map_err(|_| ProgramError::InvalidAccountData)?;
+            msg!("Token mint validated. Supply {}", mint_data.supply);
+
+            //create PDA for faucet config
+            let faucet_config_seed = b"faucet_config";
+            let (faucet_config_pda, bump_seed) =
+                Pubkey::find_program_address(&[faucet_config_seed], program_id);
+
+            // Verify the passed account is the correct PDA
+            if faucet_config_pda != *faucet_config_account.key {
+                msg!("Faucet config account is not the correct PDA");
+                return Err(ProgramError::InvalidAccountData);
+            }
+
+            //calculate required space for FaucetConfig
+            let config_data = FaucetConfig {
+                admin: *admin_account.key,
+                token_mint: *token_mint_account.key,
+                tokens_per_claim,
+                cooldown_seconds,
+                is_active: true,
+            };
+
+            let required_space = borsh::to_vec(&config_data)?.len();
+
+            //lamports for rent exemption
+            let rent = Rent::get()?;
+            let required_lamports = rent.minimum_balance(required_space);
+
+            //creating the faucet config
+            let create_account_instruction = system_instruction::create_account(
+                admin_account.key,         //payer
+                faucet_config_account.key, //new account
+                required_lamports,         //lamports to transfer
+                required_space as u64,     //allocate the space
+                program_id,                //owner of new account
+            );
+
+            //invoke system program to create the new account (CPI)
+            invoke_signed(
+                &create_account_instruction,
+                &[
+                    admin_account.clone(),
+                    faucet_config_account.clone(),
+                    system_program.clone(),
+                ],
+                &[&[faucet_config_seed, &[bump_seed]]], //PDA sign
+            )?;
+
+            //serialize and store the config of faucet
+            config_data.serialize(&mut &mut faucet_config_account.data.borrow_mut()[..])?;
+
+            msg!("Faucet initialized successfully!");
+            msg!("Admin: {}", admin_account.key);
+            msg!("Token Mint: {}", token_mint_account.key);
+            msg!("PDA of Faucet: {}", faucet_config_pda);
         }
         FaucetInstruction::ClaimTokens => {
             todo!("Claim Tokens");
