a748d88b162f7645808c4b554b0d035fbd297c05
saatvik-10 <saatvikmadan529@gmail.com>
2025-08-02 20:46:45 +0530

    updated faucet test

--- token-faucet-backend/tests/test_faucet.rs
+++ token-faucet-backend/tests/test_faucet.rs
@@ -1,5 +1,6 @@
 use borsh::BorshSerialize;
 use solana_program::{
+    example_mocks::solana_sdk::system_instruction,
     instruction::{AccountMeta, Instruction},
     pubkey::Pubkey,
     system_program,
@@ -8,6 +9,7 @@ use solana_program_test::*;
 use solana_sdk::{
     //provides transaction building tools
     account::Account,
+    program_pack::Pack,
     signature::{Keypair, Signer},
     sysvar::recent_blockhashes,
     transaction::Transaction,
@@ -51,4 +53,44 @@ async fn test_initialize_faucet() {
     println!("Starting test blockchin via admin account");
     println!("Admin address: {}", admin_keypair.pubkey());
     println!("Token mint address: {}", mint_keypair.pubkey());
+
+    //creating the SPL token mint
+
+    //calculating how much SOL we need for the mint account
+    let mint_rent = banks_client.get_rent().await.unwrap();
+    let mint_lamports = mint_rent.minimum_balance(Mint::LEN);
+
+    //creating the mint account i.e. allocating the space in blockchain
+    let create_mint_ix = system_instruction::create_account(
+        &payer.pubkey(),        //paying for the account
+        &mint_keypair.pubkey(), //address of the new account
+        mint_lamports,          //SOL for rent exemption
+        Mint::LEN as u64,       //space to allocate
+        &spl_token::id(),       //SPL token program -> who owns the account
+    );
+
+    //initializing the mint
+    let init_mint_ix = initialize_mint(
+        &spl_token::id(),        //SPL
+        &mint_keypair.pubkey(),  //mint that I just created
+        &admin_keypair.pubkey(), //mint authority who can mint new tokens
+        None,                    //users can't be frozen
+        6,                       //USDC like decimals
+    )
+    .unwrap();
+
+    //sending both instructions inside one transaction
+    let mut transaction = Transaction::new_with_payer(
+        &[create_mint_ix, init_mint_ix],
+        Some(&payer.pubkey()), //one who will pay the fees
+    );
+    transaction.sign(&[&payer, &mint_keypair], recent_blockhash);
+
+    //execting the transaction
+    let res = banks_client.process_transaction(transaction).await;
+    assert!(res.is_ok(), "Failed to create token mint: {:?}", res);
+
+    println!("Token mint has been created successfully");
+    println!("Mint authority is: {}", admin_keypair.pubkey());
+    println!("Decimals: 6");
 }
