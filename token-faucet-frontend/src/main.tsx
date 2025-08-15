186963780bf348bcbbe26fada02b418af53460e3
saatvik-10 <saatvikmadan529@gmail.com>
2025-08-15 19:15:35 +0530

    added provider and type correction

--- token-faucet-frontend/src/main.tsx
+++ token-faucet-frontend/src/main.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './App.css';
-    import '@solana/wallet-adapter-react-ui/styles.css';
+import WalletContextProvider from './context/WalletContextProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <WalletContextProvider>
+      <App />
+    </WalletContextProvider>
   </StrictMode>
 );
