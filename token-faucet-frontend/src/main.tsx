9b5d51f12d82d212d7406ffda7f7973bc229b44d
saatvik-10 <saatvikmadan529@gmail.com>
2025-08-15 20:52:28 +0530

    added toast/ context

--- token-faucet-frontend/src/main.tsx
+++ token-faucet-frontend/src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './App.css';
 import WalletContextProvider from './context/WalletContextProvider.tsx';
+import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <WalletContextProvider>
+      <Toaster />
       <App />
     </WalletContextProvider>
   </StrictMode>
