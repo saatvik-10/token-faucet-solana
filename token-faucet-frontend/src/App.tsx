1f4f8685ccd3880d5a6cd3c3f3404e2ce412487c
saatvik-10 <saatvikmadan529@gmail.com>
2025-08-16 22:30:50 +0530

    added initialize faucet

--- token-faucet-frontend/src/App.tsx
+++ token-faucet-frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { WalletConnection } from './components/WalletConnection';
 import { FaucetStats } from './components/FaucetStats';
+import { InitializeFaucet } from './components/InitializeFaucet';
 
 const App: React.FC = () => {
   return (
-    <div>
-      <WalletConnection />
+    <div className='min-h-screen bg-gradient-to-br from-indigo-900 via-purple-900 to-pink-800'>
+      <div className='container mx-auto px-4 py-8 space-y-8'>
+        <WalletConnection />
 
-      <div className='mt-8'>
+        <InitializeFaucet />
+
         <FaucetStats />
       </div>
     </div>
