a4ae5dff6d6f6ffed67b24bc9fcd8c9b542ddbf0
saatvik-10 <saatvikmadan529@gmail.com>
2025-08-16 22:30:42 +0530

    fixed buffer pollyfill

--- token-faucet-frontend/vite.config.ts
+++ token-faucet-frontend/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [tailwindcss(), nodePolyfills()],
 });
